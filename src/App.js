b480ebd66d488dddf9e60f0f7c88dcedcf34b22b
Rahul <rahulmandal2k21@gmial.com>
2024-06-16 13:40:45 +0530

    first

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ useEffect(()=>{
 
 
   return (
-  <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-8 bg-gray-800 text-orange-500">
+  <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-8 bg-orange-800 text-orange-500">
     <h1 className="text-white text-center my-3">
       Password Generator
     </h1>
@@ -51,17 +51,17 @@ useEffect(()=>{
 
         <div className="flex items-center gap-x-1">
           <input type="range" min={6} max={100} value={length}  className="cursor-pointer" onChange={(e)=>setLength(e.target.value)} name="" id=""  />
-          <label htmlFor='length'>Length: {length} </label>
+          <label className='text-white' htmlFor='length'>Length: {length} </label>
 
         </div>
         <div className="flex items-center gap-x-1">
          <input  type="checkbox" name=""  id=""  defaultChecked={numberALlowed}  onChange={()=>{setNumberAllowed((prev)=>!prev)}}/>
-         <label htmlFor='number'>Numbers </label>
+         <label  className='text-white' htmlFor='number'>Numbers </label>
 
         </div>
         <div className="flex items-center gap-x-1">
          <input  type="checkbox" name=""  id=""  defaultChecked={charAllowed}  onChange={()=>{setCharAllowed((prev)=>!prev)}}/>
-         <label htmlFor='charInput'>Character </label>
+         <label className='text-white' htmlFor='charInput'>Character </label>
 
         </div>
       </div>
